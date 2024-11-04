3bef247a7ffda9a7241589f1ce5a7854800ef492
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-04 17:58:51 -0300

    update src/schemas/UserSchema.js

--- src/schemas/UserSchema.js
+++ src/schemas/UserSchema.js
@@ -3,7 +3,8 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   ID: {type: String, required: true},
   phoneNumber: {type: String, default: ""},
-  lastAd: {type: Number, default: 0}
+  status: {type: String, default: ""},
+  lastAd: {type: Number, default: 0},
 })
 
 module.exports = model("Users", UserSchema);
