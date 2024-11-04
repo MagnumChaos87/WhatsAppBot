c93d419f410adb798bc7691959595be1e3b4927b
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-04 16:34:44 -0300

    update src/events/message.js

--- src/events/message.js
+++ src/events/message.js
@@ -1,12 +1,24 @@
+const { UserSchema } = require("../schemas/UserSchema");
+
 module.exports = {
   name: "message",
   async execute(message, client) {
     try {
-      console.log(message);
+      console.log(client.me);
+      
+      if (message.from === client.me) return;
+      
+      let User = await UserSchema.findOne({
+        ID: message.from
+      });
+      
+      if (!User) User = await UserSchema.create({
+        ID: message.from
+      });
       
       if (message.body.toLowerCase() === "oi") message.reply("Boa Tarde");
       
-      if (message.body.toLowerCase() === "tchau") message.reply("Até logo, até mais ver, bon voyage, arrivederci, até mais, adeus, boa viagem, vá em paz, que a porta bata onde o sol não bate, não volte mais aqui, hasta la vista baby, escafeda-se, e saia logo daqui")
+      if (message.body.toLowerCase() === "tchau") message.reply("Até logo, até mais ver, bon voyage, arrivederci, até mais, adeus, boa viagem, vá em paz, que a porta bata onde o sol não bate, não volte mais aqui, hasta la vista baby, escafeda-se, e saia logo daqui");
     } catch(err) {
       console.log(err)
     }
