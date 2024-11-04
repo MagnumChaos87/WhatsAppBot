74edb8b8e249f3c5e8535d34e2bc22ed11d02337
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-04 16:32:37 -0300

    update src/events/ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
+const UserSchema = require("../schemas/UserSchema");
+
+require("dotenv").config();
+
 module.exports = {
   name: "ready",
   async execute(client) {
     try {
       console.log("ZeusBolt: Conectado.");
-      /*
-      await mongoose.connect(mongoToken + "invite", {
+      
+      await mongoose.connect(process.env.MONGO_TOKEN + "advertisement", {
         useNewUrlParser: true,
         useUnifiedTopology: true
         }).then(() => {
@@ -19,16 +23,37 @@ module.exports = {
       mongoose.connection.on("disconnected", async () => {
         console.log("Conexão com o MongoDB perdida. Tentando reconectar...");
         
-        await mongoose.connect(mongoToken + "invite", {
+        await mongoose.connect(process.env.MONGO_TOKEN + "advertisement", {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         }).then(() => {
-          console.log("Conexão com MongoDB reestabelecida com sucesso!")
+          console.log("Conexão com MongoDB reestabelecida com sucesso!");
         }).catch(err => {
-          console.log(`Falha ao tentar reconectar!\n\n${err}`)
+          console.log("Falha ao tentar reconectar!", err);
         })
       });
-      */
+      
+      const second = 1000;
+      const minute = 60 * second;
+      const hour = 60 * minute;
+      const day = 24 * hour;
+      const week = 7 * day;
+      
+      setInterval(() => {
+        await sendAd();
+      }, second);
+      
+      async function sendAd() {
+        const Users = await UserSchema.find().then(async (Users) => {
+          return Array.from(Users.values())
+        })
+        
+        for (User of Users) {
+          if (User.lastAd + week > Date.now()) continue;
+          
+          client.sendMessage(User.ID, "Imagine um Anúncio aqui!");
+        }
+      }
     } catch(err) {
       console.log(err)
     }
