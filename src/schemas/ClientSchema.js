f8ed14fc702328217e0a6cf346b76522a2359ab5
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-05 15:47:48 -0300

    Delete the file at 'src/schemas/ClientSchema.js'

--- src/schemas/ClientSchema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const ClientSchema = new Schema({
-  ID: {type: String, required: true},
-  commands: {type: Array, default: []},
-  prefix: {type: String, default: "!"}
-});
-
-module.exports = model("Clients", ClientSchema);
