9636bae9591a9d3ed4a6de24756e51fe512b113b
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-08 10:37:26 -0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     args: ["--headless", "--no-sandbox", "--no-first-run", "--disable-gpu", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
-    headless: false 
+    headless: true 
   }
 });
 
