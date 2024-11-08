46ee57c68839e43171a1b7ccc79cb8a2a2999440
Magnum Chaos <138801763+MagnumChaos87@users.noreply.github.com>
2024-11-08 15:55:59 -0300

    update src/commands/test.js

--- src/commands/test.js
+++ src/commands/test.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   async execute(message, args, client) {
     try {
-      const phoneNumberId = "+554792377108";
+      const phoneNumberId = "1153130346429745";
       
       const recipientNumber = "+5521983196551";
       
